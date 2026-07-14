fec240404350dcd952332a4f6bacd3e291e1784c
Anusri <tallaanusri@gmail.com>
2026-07-14 14:03:31 +0530

    Update AMUX2_3V.v

--- inputfiles/AMUX2_3V.v
+++ inputfiles/AMUX2_3V.v
@@ -1,1 +1,15 @@
-2
+//AI Generated Macro Blackbox Stub Code
+
+module AMUX2_3V (
+    input  wire I0,
+    input  wire I1,
+    input  wire select,
+    output wire out
+);
+
+    // Functional behavioral model
+    assign out = (select === 1'b1) ? I1 :
+                 (select === 1'b0) ? I0 :
+                 1'bx;
+
+endmodule
